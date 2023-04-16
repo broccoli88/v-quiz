2e4ce76078535f03af127dc1c8e2291a2a0d72fa
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-04-16 15:47:56 +0200

    Start of Quiz app
    main component + card component + search func

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { ref, watch } from "vue";
+import q from "./data/quizes.json";
+import Card from "./components/Card.vue";
+
+const quizes = ref(q);
+const search = ref(``);
+
+watch(search, () => {
+    quizes.value = q.filter((quiz) =>
+        quiz.name.toLowerCase().includes(search.value.toLowerCase())
+    );
+});
+</script>
+<template>
+    <div class="container">
+        <header>
+            <h1>Quiz</h1>
+            <input v-model.trim="search" type="text" placeholder="Serch..." />
+        </header>
+        <div class="options-container">
+            <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+            <!-- <div class="card" v-for="quiz in quizes" :key="quiz.id">
+                <img :src="quiz.img" alt="" />
+                <div class="card-text">
+                    <h2>{{ quiz.name }}</h2>
+                    <p>{{ quiz.questions.length }} questions</p>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</template>
+<style scoped>
+.container {
+    max-width: 1000px;
+    margin: 0 auto;
+}
+
+header {
+    margin: 10px 0 30px;
+    display: flex;
+    align-items: center;
+}
+
+header h1 {
+    margin-right: 30px;
+    font-weight: bold;
+}
+
+header input {
+    border: none;
+    background-color: rgba(128, 128, 128, 0.1);
+    padding: 10px;
+    border-radius: 5px;
+    color: var(--color-text);
+}
+
+.options-container {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 40px;
+}
+</style>
